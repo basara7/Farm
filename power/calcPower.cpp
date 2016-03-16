3a3db1499df9b03deefd477c671040f9e9550290
kirax10a6@126.com <miaoya@ad4.ad.alcatel.com>
2016-03-16 18:05:37 +0800

    20160316_02

--- power/calcPower.cpp
+++ power/calcPower.cpp
@@ -1,9 +1,18 @@
-//3d10-1 动态规划 背包问题 
+//动态规划 背包问题 
 //#include "stdafx.h"
 #include <iostream>   
-using namespace std;   
-
-const int N = 4; 
+#include<cstdio>
+#include<cstring>
+using namespace std;
+const int INF = 0x3f3f3f3f;
+const int MAXL = 500+5;
+const int totalW = 60;
+const int n = 2;
+const int value[2] = {1, 30};
+const int weight[2]= {1, 50};
+int dp[10005];
+int min(int a, int b) { return a < b ? a : b; }
+ 
 const int NODES = (24*60/15);  //96个节点
 const double POWER[37][2] ={{300, 354.28},
 {320, 346.31},
@@ -42,8 +51,6 @@ const double POWER[37][2] ={{300, 354.28},
 {980, 295.27},
 {1000, 297.23},
 {1020, 298.2}};
-void Knapsack(int v[],int w[],int c,int n,int m[][10]); 
-void Traceback(int m[][10],int w[],int c,int n,int x[]); 
 
 bool getCoalConsumptionFromPower(int power, double &coalConsumption);
 
@@ -54,100 +61,42 @@ bool getCoalConsumptionFromPower(int power, double &coalConsumption);
 //  例如：达到340后第一个15分钟， 煤耗为 343.14 * （ 1+ 1%）
 //4. 每天出力一定，可手动输入某个时间段的出力值，由程序规划剩余出力，以实现煤耗最少
 //  例如： 从1点到6点，设定最低出力320，剩余由程序规划
-int main() 
-{ 
-    int c=8;
-    int v[]={0,2,1,4,3},w[]={0,1,4,2,3};//下标从1开始 
-    int x[N+1]; 
-    int m[10][10]; 
 
-    cout<<"待装物品重量分别为："<<endl; 
-    for(int i=1; i<=N; i++) 
-    { 
-        cout<<w[i]<<" "; 
-    } 
-    cout<<endl; 
+int main(){
+        dp[0] = 0;
+		int bag[MAXL][MAXL] ={0};
+        for(int i = 1; i <= totalW; i++) dp[i] = INF;
+        //---
+        for(int i = 0; i < n; i++){
+            for(int j = weight[i]; j <= totalW; j++){
+                    dp[j] = min(dp[j-weight[i]]+value[i], dp[j]);
+					bag[i+1][j] = 1;
+            }
+        }
+		
+        if(dp[totalW] == INF) 
+		{
+			printf("This is impossible.\n");
+		}
+        else
+		{			
+			printf("The minimum amount of money in the piggy-bank is %d.\n", dp[totalW]);
+			int i = n, j = totalW;  
+			while(i > 0 && j > 0)  
+            {  
+               if(bag[i][j] == 1)  
+               {
+                  cout << weight[i-1] << " ";  
+                  j -= weight[i-1];  
+               }  
+               else  
+                  i--;  
+            }  
+		    cout << endl; 
+		}
+		
 
-    cout<<"待装物品价值分别为："<<endl; 
-    for(int i=1; i<=N; i++) 
-    { 
-        cout<<v[i]<<" "; 
-    } 
-    cout<<endl; 
-
-	cout<<"背包能装的最大容量为："<<c<<endl;
-    Knapsack(v,w,c,N,m); 
-
-    cout<<"背包能装的最大价值为："<<m[1][c]<<endl; 
-
-    Traceback(m,w,c,N,x); 
-    cout<<"背包装下的物品编号为："<<endl; 
-    for(int i=1; i<=N; i++) 
-    { 
-        if(x[i]==1) 
-        { 
-            cout<<i<<" "; 
-        } 
-    } 
-    cout<<endl; 
-	
-	//test
-	double coalConsumption = 0.0;
-	getCoalConsumptionFromPower(780, coalConsumption);
-	cout<<"coalConsumption = " <<coalConsumption<<endl; 
-
-    return 0; 
-} 
-
-void Knapsack(int v[],int w[],int c,int n,int m[][10]) 
-{ 
-    int jMax = min(w[n]-1,c);//背包剩余容量上限 范围[0~w[n]-1] 
-    for(int j=0; j<=jMax;j++) 
-    { 
-        m[n][j]=0; 
-    } 
-
-    for(int j=w[n]; j<=c; j++)//限制范围[w[n]~c] 
-    { 
-        m[n][j] = v[n]; 
-    } 
-
-    for(int i=n-1; i>1; i--) 
-    { 
-        jMax = min(w[i]-1,c); 
-        for(int j=0; j<=jMax; j++)//背包不同剩余容量j<=jMax<c 
-        { 
-            m[i][j] = m[i+1][j];//没产生任何效益 
-        } 
-
-        for(int j=w[i]; j<=c; j++) //背包不同剩余容量j-wi >c 
-        { 
-            m[i][j] = max(m[i+1][j],m[i+1][j-w[i]]+v[i]);//效益值增长vi   
-        } 
-    } 
-    m[1][c] = m[2][c]; 
-    if(c>=w[1]) 
-    { 
-        m[1][c] = max(m[1][c],m[2][c-w[1]]+v[1]); 
-    } 
-} 
-
-//x[]数组存储对应物品0-1向量,0不装入背包，1表示装入背包 
-void Traceback(int m[][10],int w[],int c,int n,int x[]) 
-{ 
-    for(int i=1; i<n; i++) 
-    { 
-        if(m[i][c] == m[i+1][c]) 
-        { 
-            x[i]=0; 
-        } 
-        else 
-        { 
-            x[i]=1; 
-            c-=w[i]; 
-        } 
-    } 
-    x[n]=(m[n][c])?1:0; 
+    return 0;
 }
 
 //Function getCoalConsumptionFromPower
